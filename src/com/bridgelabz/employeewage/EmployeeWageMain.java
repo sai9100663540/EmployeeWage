98d310424ba7eaa9d7a186bdd8a0d0b4cf849d7f
saikumar <saikumargampa5@gmail.com>
2023-01-13 19:12:18 +0530

    Added Welcome Message on Master branch

---
+++ src/com/bridgelabz/employeewage/EmployeeWageMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.employeewage;
+
+public class EmployeeWageMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee wage");
+
+    }
+}
